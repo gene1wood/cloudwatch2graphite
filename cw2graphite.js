328cc448df7db73b7691721fa1e38fdfc3283c7b
Jared Hirsch <ohai@6a68.net>
2013-05-20 12:28:16 -0700

    insane spacing makes eyes bleed. fix.

--- cw2graphite.js
+++ cw2graphite.js
@@ -13,22 +13,17 @@ var cloudwatch = new CloudWatch({
     'secretAccessKey' : global_options.credentials.secretAccessKey,
     'region'          : global_options.metrics_config.region
 });
-
 var interval = global_options.metrics_config.interval_minutes;
-
 var metrics = global_options.metrics_config.metrics
-
 for(index in metrics) {
 	getOneStat(metrics[index]);
 }
 
 function getOneStat(metric) {
-
 	var now = new Date();
 	var then = (interval).minutes().ago()
 	var end_time = dateFormat(now, "isoUtcDateTime");
 	var start_time = dateFormat(then, "isoUtcDateTime");
-
   var dimensions = [];
   dimensions.push({
     Name: metric["Dimensions.member.1.Name"],
@@ -50,36 +45,24 @@ function getOneStat(metric) {
 		Unit: [metric.Unit],
 		Dimensions: dimensions
 	}
-
 	metric.name = (global_options.metrics_config.carbonNameSpacePrefix != undefined) ? global_options.metrics_config.carbonNameSpacePrefix + "." : "";
-
 	metric.name += metric.Namespace.replace("/", ".");
 	metric.name += "." + metric["Dimensions.member.1.Value"];
 	metric.name += "." + metric.MetricName;
 	if (metric["Dimensions.member.2.Value"]!==undefined) 
 		metric.name += "." + metric["Dimensions.member.2.Value"];
-
-
-	
 	metric.name += "." + metric["Statistics.member.1"];
 	metric.name += "." + metric.Unit;
-
 	metric.name = metric.name.toLowerCase()
 
 	cloudwatch.GetMetricStatistics(options, function(error, response) {
-
-
-		if(error) {
+		if (error) {
 			console.error("ERROR ! ",JSON.stringify(error));
-
 		} else {
-
-
 			var memberObject = response.Body.GetMetricStatisticsResponse.GetMetricStatisticsResult.Datapoints.member;
 			if (memberObject != undefined) {
-
 				var memberObj;
-				if(memberObject.length === undefined) {
+				if (memberObject.length === undefined) {
 					memberObj = memberObject; 
 				} else {
 					// samples might not be sorted in chronological order
@@ -89,10 +72,8 @@ function getOneStat(metric) {
 					});
 					memberObj = memberObject[memberObject.length - 1];
 				}
-
 				metric.value = memberObj[metric["Statistics.member.1"]]
 				metric.ts = parseInt(new Date().getTime(memberObj.TimeStamp) / 1000);
-
         var m = {};
         m[metric.name] = metric.value;
         // TODO this is terrible. rather than have a separate queue of in-flight
@@ -105,22 +86,16 @@ function getOneStat(metric) {
           if (err) console.log('ERROR! Failed to write to graphite server: ' + err);
           client.end();
         });
-
-				
 				console.log("%s %s %s", metric.name, metric.value, metric.ts);
-
 				if ((metric === undefined)||(metric.value === undefined)) {
 					console.dir(response);
 					console.dir(response.GetMetricStatisticsResult.Datapoints.member);
 					console.log("[1]")
 					console.dir(response.GetMetricStatisticsResult.Datapoints.member[1]);
 					console.log("length=" + response.GetMetricStatisticsResult.Datapoints.member.length);
-
 					console.log(typeof response.GetMetricStatisticsResult.Datapoints.member);
-
 				}
 			} //if(memberObject != undefined)
-
 		}
 	});
 }
